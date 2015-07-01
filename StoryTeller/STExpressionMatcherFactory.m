7d99cdbf88112728d4ee54edca32b75f4ceea8da
Derek Clarkson <d4rkf1br3@gmail.com>
2015-07-02 00:03:07 +1000

    Testing

--- StoryTeller/STExpressionMatcherFactory.m
+++ StoryTeller/STExpressionMatcherFactory.m
@@ -60,8 +60,8 @@ typedef NS_ENUM(NSUInteger, ValueType) {
 #pragma mark - Logger control
 
 -(void) parser:(PKParser * __nonnull)parser didMatchLogAll:(PKAssembly * __nonnull)assembly {
+    NSLog(@"st %@", [STStoryTeller storyTeller] == nil ? @"Nil": @"Created");
     [[STStoryTeller storyTeller] logAll];
-    NSLog(@"Looging evverrything");
     mflog(@"set LogAll");
 
 }
